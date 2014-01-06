3773296667575aa374be62f72c0301547f00fe48
nastaran <nastaran.shafiei@gmail.com>
2014-01-06 13:12:45 -0800

    Introduced the new property start_with_target0, if it is set to true, jpf-nas explores only those path that starts with the process set to target.0. This is useful to trim out uninteresting execution paths, for example those systems require server always starts first

--- src/main/nas/NasVM.java
+++ src/main/nas/NasVM.java
@@ -4,12 +4,18 @@ import nas.java.net.choice.Scheduler;
 import nas.java.net.connection.ConnectionManager;
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.JPF;
+import gov.nasa.jpf.vm.ChoiceGenerator;
 import gov.nasa.jpf.vm.MultiProcessVM;
+import gov.nasa.jpf.vm.ThreadInfo;
+import gov.nasa.jpf.vm.choice.MultiProcessThreadChoice;
 
 public class NasVM extends MultiProcessVM{
-
+  boolean startWithTarget0;
+  
   public NasVM (JPF jpf, Config conf) {
     super(jpf, conf);
+    
+    startWithTarget0 = config.getBoolean("vm.nas.start_with_target0", false);
   }
 
   @Override
@@ -17,5 +23,15 @@ public class NasVM extends MultiProcessVM{
     super.initSubsystems(config);
     Scheduler.init(config);
     ConnectionManager.init();
+  }
+  
+  @Override
+  protected ChoiceGenerator<?> getInitialCG () {
+    if(!startWithTarget0) {
+      return super.getInitialCG();
+    } else {
+      ThreadInfo[] runnables = {getThreadList().getAllMatching(vm.getTimedoutRunnablePredicate())[0]};
+      return new MultiProcessThreadChoice("<root>", runnables, true);
+    }
   }
 }
